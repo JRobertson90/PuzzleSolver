9d841b5decef3c2687f9fbdf90f77c872222fd01
Jason Robertson <jayperdu@gmail.com>
2014-07-11 22:13:59 -0600

    second commit

---
+++ src/puzzle/Spot.java
@@ -0,0 +1,13 @@
+package puzzle;
+
+public class Spot {
+
+    private int x;
+    private int y;
+
+    public Spot() {}
+    public Spot(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
